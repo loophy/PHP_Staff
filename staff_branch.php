0767ccc35fa7b65b49da63234443bd932dc1cd18
grnboy <grnboy.hey@gmail.com>
2014-01-15 23:25:43 +0900

    modify 導線調整

--- staff_branch.php
+++ staff_branch.php
@@ -6,11 +6,11 @@ if (isset($_POST['disp'])==true)
 		header('Location:staff_ng.php');
 	}
 	$staff_code=$_POST['staffcode'];
-	header('Location:staff_dispd.php?staffcode='.$staff_code);	
+	header('Location:staff_disp.php?staffcode='.$staff_code);	
 }
 if (isset($_POST['add'])==true)
 {
-	header('Location;staff_add.php');
+	header('Location:staff_add.php');
 }
 if (isset($_POST['edit'])==true)
 {
