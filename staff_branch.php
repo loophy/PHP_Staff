b15c9e0954b64630dae3209c250204fb0f0330ac
grnboy <grnboy.hey@gmail.com>
2014-01-15 21:39:03 +0900

    modify スタッフ参照画面への導線設置

--- staff_branch.php
+++ staff_branch.php
@@ -1,4 +1,13 @@
 <?php
+if (isset($_POST['disp'])==true)
+{
+	if (isset($_POST['staffcode'])==false)
+	{
+		header('Location:staff_ng.php');
+	}
+	$staff_code=$_POST['staffcode'];
+	header('Location:staff_dispd.php?staffcode='.$staff_code);	
+}
 if (isset($_POST['add'])==true)
 {
 	header('Location;staff_add.php');
