a31854cfdfbf59212f80c516aaa4a9b8b7fc19b5
grnboy <grnboy.hey@gmail.com>
2014-01-15 21:38:44 +0900

    add スタッフ参照画面追加

---
+++ staff_disp.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>ろくまる農園</title>
+</head>
+<body>
+	<?php
+
+	try
+	{
+		$staff_code=$_GET['staffcode'];
+
+		$dsn='mysql:dbname=shop;host=localhost';
+		$user='root';
+		$password='';
+		$dbh=new PDO($dsn, $user, $password);
+		$dbh->query('SET NAMES utf8');
+
+		$sql='SELECT name FROM mst_staff WHERE code=?';
+		$stmt=$dbh->prepare($sql);
+		$data[]=$staff_code;
+		$stmt->execute($data);
+
+		$rec=$stmt->fetch(PDO::FETCH_ASSOC);
+		$staff_name=$rec['name'];
+
+		$dbh=null;
+	}
+	catch (Exception $e)
+	{
+		print 'ただいま障害により大変おg名枠をお掛けしております。';
+		exit();
+	}
+
+	?>
+	スタッフ情報参照<br />
+	<br />
+	スタッフコード<br />
+	
+	<?php
+		print $staff_code;
+	?>
+	<br />
+	スタッフ名<br />
+	<?php print $staff_name ?>
+	<br />
+	<br />
+	<form>
+		<input type="button" onclick="history.back()" value="戻る">
+	</form>
+
+</body>
+</html>
