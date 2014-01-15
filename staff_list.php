f86af3c06ebb0bc61540931158f4f5ddb51ce979
grnboy <grnboy.hey@gmail.com>
2014-01-15 17:15:53 +0900

    first commit

---
+++ staff_list.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>ろくまる農園</title>
+</head>
+<body>
+
+	<?php
+
+	try
+	{
+		$dsn='mysql:dbname=shop;host=localhost';
+		$user='root';
+		$password='';
+		$dbh=new PDO($dsn, $user, $password);
+		$dbh->query('SET NAMES utf8');
+
+		$sql='SELECT code,name FROM mst_staff WHERE 1';
+		$stmt=$dbh->prepare($sql);
+		$stmt->execute();
+
+		$dbh=null;
+
+		print 'スタッフ一覧<br /><br />';
+
+		print '<form method="post" action="staff_branch.php">';
+		while(true)
+		{
+			$rec=$stmt->fetch(PDO::FETCH_ASSOC);
+			if($rec==false)
+			{
+				break;
+			}
+			print '<input type="radio" name="staffcode" value="'.$rec['code'].'">';
+			print $rec['name'];
+			print '<br />';
+		}
+		print '<input type="submit" name="edit" value="修正">';
+		print '<input type="submit" name="delete" value="削除">';
+		print '</form>';
+	}
+
+	catch (Exception $e)
+	{
+		print 'ただいま障害により大変ご迷惑をおかけしております。';
+		exit();
+	}
+
+	?>
+
+</body>
+</html>
